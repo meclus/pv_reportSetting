2ccc87d54b4728785aa614e8d20813e6d5c63985
mickey.wang <mickey.wang.live@outlook.com>
2019-08-01 19:12:12 +0800

    添加项目文件。

---
+++ DbConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PvConfig
+{
+    public class DbConfig
+    {
+        public string Host { get; set; }
+        public string port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Database { get; set; }
+        public bool Default { get; set; }
+    }
+}
---
+++ todo.md
@@ -0,0 +1,6 @@
+﻿TODO list
+===
+
+1. config db
+2. get all page list
+3. get page's field settings
